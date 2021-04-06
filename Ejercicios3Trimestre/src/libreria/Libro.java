87d7305b24b227b17f4ca9e89ba3f69896a0688b
Javi <javiersmr471@gmail.com>
2021-04-06 09:59:47 +0200

    Empezado ejercicio libreria

---
+++ Ejercicios3Trimestre/src/libreria/Libro.java
@@ -0,0 +1,10 @@
+package libreria;
+
+public class Libro extends Publicacion{
+	private boolean prestado;
+
+	public Libro() {
+		super
+	}
+	
+}
---
+++ Ejercicios3Trimestre/src/libreria/Publicacion.java
@@ -0,0 +1,27 @@
+package libreria;
+
+public class Publicacion {
+	private int cod;
+	private String titulo;
+	private Tematica tematica;
+	private int anoPublicacion;
+	public Publicacion(int cod, String titulo, Tematica tematica, int anoPublicacion) {
+		super();
+		this.cod = cod;
+		this.titulo = titulo;
+		this.tematica = tematica;
+		this.anoPublicacion = anoPublicacion;
+	}
+	@Override
+	public String toString() {
+		return "Publicacion [cod=" + cod + ", titulo=" + titulo + ", anoPublicacion=" + anoPublicacion + "]";
+	}
+	public int getCod() {
+		return cod;
+	}
+	public int getAnoPublicacion() {
+		return anoPublicacion;
+	}
+	
+	
+}
